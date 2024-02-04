30b9c17974102df5b8c233445dac86051c1521ad
rafaapcode <rafaap2003@gmail.com>
2024-02-04 08:24:04 -0300

    feat: creating the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,20 @@
+import { SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-    return <div>Header</div>
+    return <header className="flex items-center justify-between">
+        <Link href="/" className="flex items-center space-x-3">
+            <div className="bg-[#0160FE] w-fit p-2">
+                <Image src="/uploadingLogo.png" width={50} height={50} alt="Logo"/>
+            </div>
+            <h1 className="font-bold text-xl">Uploading App</h1>
+        </Link>
+        <div className="px-5 flex items-center space-x-2">
+            <UserButton afterSignOutUrl="/"/>
+            <SignedOut>
+                <SignInButton afterSignInUrl="/dashboard" mode="modal"/>
+            </SignedOut>
+        </div>
+    </header>
 };
